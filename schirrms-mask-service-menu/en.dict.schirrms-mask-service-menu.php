e70b2441dcc57455084b5c84e691474c40e88bb2
Schirrms <schirrms@schirrms.net>
2021-02-16 15:45:35 +0100

    Initial version (0.0.1.0)

---
+++ schirrms-mask-service-menu/en.dict.schirrms-mask-service-menu.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2020 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+));
+?>
